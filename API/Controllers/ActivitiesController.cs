bfb9b62bb89b7cdd84add41606cced56e4e7b8b9
sai wentum <michael.wentum@gmail.com>
2023-01-21 11:12:40 -0500

    First commit message

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,37 @@
+
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ActivitiesController : BaseApiController
+    {
+
+        private readonly DataContext _context;
+
+        public ActivitiesController(DataContext context)
+        {
+            _context = context;
+
+
+        }
+
+        [HttpGet] //api/activities
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await _context.Activities.ToListAsync();
+
+        }
+
+        [HttpGet("{id}")] //api/activities/id
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+            return await _context.Activities.FindAsync(id);
+
+        }
+    }
+}
